e8305657c79df1407426d193c88b996ab5c23018
rhit-kampweke <kampweke@rose-hulman.edu>
2025-05-09 14:08:38 -0400

    Updated plant sensors
    Just modified water level to give back percentage

--- PlantSensors.cpp
+++ PlantSensors.cpp
@@ -1,10 +1,11 @@
 // Following is code for ECE436 Collecting sensor values for Plant pot
 // By Kaitlyn Kampwerth
-/*
+/*he aud
  * The code will grab the sensor values of water level and soil moisture level
  */
 
 #include <Arduino.h> 
+#include <math.h>
 #include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
 
 DHTesp dht;
@@ -16,9 +17,29 @@ int soilPin;
 
 int getWaterLevel() {
   int waterValue = analogRead(levelPin);
-  Serial.print("Water Level: ");
+  Serial.print("Water Level from Sensor: ");
   Serial.println(waterValue);
-  return waterValue;
+  if (waterValue < 1000){
+    Serial.println("you are below 1000");
+    return 0;
+  }
+  // Goes up to a max of 37% before going to next
+  else if (waterValue < 1900){
+    double mathValue = 0.04089*waterValue-39.8;
+    Serial.println("you are above 1000");
+    return mathValue;
+  }
+  else {
+    double OtherMathValue = 0.834*waterValue - 1693;
+    Serial.println("you are above 1900");
+    if (OtherMathValue > 100){
+      return 100;
+    }
+    else if (OtherMathValue < 37){
+      return 37;
+    }
+    return OtherMathValue; 
+  }
 }
 
 int getSoilMoisture() {
